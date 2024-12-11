adc9f77c34452a4bb32c1656ba0974eac548aee8
Jesse Cooke <jesse@relativepath.io>
2024-12-10 20:20:40 -0800

    Add LICENSE & package details

---
+++ lib/ntfy.ex
@@ -0,0 +1,18 @@
+defmodule Ntfy do
+  @moduledoc """
+  Documentation for `Ntfy`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ntfy.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ntfy_test.exs
@@ -0,0 +1,8 @@
+defmodule NtfyTest do
+  use ExUnit.Case
+  doctest Ntfy
+
+  test "greets the world" do
+    assert Ntfy.hello() == :world
+  end
+end
